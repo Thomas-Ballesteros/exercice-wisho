ad6e949423812bd1f936603ce97c6639b97e39f5
LAPTOP-8LRM34JR\orolo <bt95240@gmail.com>
2022-05-09 11:44:41 +0200

    connection DB

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require ('express'); // Nommé le framwork 'express'
-const app = express();// appelle di framwork express
+const app = express();// appel du framwork express
 require('./models/dbConfig');
 //conection au serveur
 app.listen(5500,() => console.log('serveur started: 5500'));
